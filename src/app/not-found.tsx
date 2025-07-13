9b9395fabf493e42936a98927eaad2b4924df5d9
amin-erfanian <dev.aminerfanian@gmail.com>
2025-07-13 15:38:36 +0330

    add not found file

---
+++ src/app/not-found.tsx
@@ -0,0 +1,14 @@
+export default function NotFound() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h1>404 – Page Not Found</h1>
+    </div>
+  );
+}
