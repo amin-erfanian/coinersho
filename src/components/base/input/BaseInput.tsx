336f2fbc5f996d66876ac28d41a598f6a6827563
amin-erfanian <dev.aminerfanian@gmail.com>
2025-07-13 11:50:27 +0330

    add base input

---
+++ src/components/base/input/BaseInput.tsx
@@ -0,0 +1,76 @@
+import React, {
+  InputHTMLAttributes,
+  ReactNode,
+  useId,
+  useState,
+  useEffect,
+} from "react";
+import classNames from "classnames";
+import styles from "./BaseInput.module.scss";
+
+interface BaseInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  name: string;
+  direction?: "ltr" | "rtl";
+  variant?: "outlined" | "text";
+  prepend?: ReactNode;
+  append?: ReactNode;
+}
+
+const BaseInput: React.FC<BaseInputProps> = ({
+  label = "",
+  name,
+  direction = "rtl",
+  variant = "text",
+  prepend,
+  append,
+  value,
+  onChange,
+  autoComplete = "off",
+  ...rest
+}) => {
+  const inputId = useId();
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    setIsActive(!!value);
+  }, [value]);
+
+  return (
+    <div className={styles.field}>
+      {prepend && <div className={styles.field__icon}>{prepend}</div>}
+
+      <div
+        className={classNames(
+          styles.field__content,
+          styles[`field__content--${variant}`]
+        )}
+      >
+        <input
+          className="field__input"
+          name={name}
+          dir={direction}
+          autoComplete={autoComplete}
+          {...rest}
+          value={value}
+          onChange={onChange}
+        />
+
+        {label && (
+          <label
+            htmlFor={inputId}
+            className={classNames(styles.field__label, {
+              [styles["field__label--has-active"]]: isActive,
+            })}
+          >
+            {label}
+          </label>
+        )}
+      </div>
+
+      {append && <div className={styles.field__icon}>{append}</div>}
+    </div>
+  );
+};
+
+export default BaseInput;
